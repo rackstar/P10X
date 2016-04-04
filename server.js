858f5bea5febdabfaa852d88d3ad081f685d6ae7
Rocky Murdoch <rockysta009@gmail.com>
2016-04-05 00:02:57 +0100

    added gulp tools as dependency

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 1337;
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(1337);
+app.listen(port);
