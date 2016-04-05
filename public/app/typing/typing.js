9de833be0562e4a41c163619d37d298f264d7513
Rocky Murdoch <rockysta009@gmail.com>
2016-04-05 20:14:59 +0100

    initial jquery set-up

--- public/app/typing/typing.js
+++ public/app/typing/typing.js
@@ -1,5 +1,38 @@
-angular.module('typing', [])
+var type = {};
 
-.controller('typingCtrl', function($scope) {
-  $scope.count = {};
+$(function () {
+  var $inputArea = $('#inputArea')
+  type.code = "for (var i = 0; i < array.length; i++)"
+  type.key;
+  type.index = 0;
+
+  type.init = function () {
+    //listener for key input
+    $inputArea.on('keydown', function (event) {
+      type.key = String.fromCharCode(event.which);
+      //compares key input to text
+      type.compare(type.key);
+    });
+
+  }
+
+  ;
+
+  type.compare = function (key) {
+    //compare with current letter
+    if (key === type.code.charAt(type.index)) {
+      //if same light up green
+      $('#' + type.index).addClass('correct');
+    } else {
+      $('#' + type.index).addClass('incorrect');
+      
+    }
+
+
+
+    //if different light up red
+
+
+  }
+
 });
