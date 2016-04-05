41e799ba76834196372e058dcf4d7814a580c29a
Rocky Murdoch <rockysta009@gmail.com>
2016-04-05 15:03:38 +0100

    added toggleTyping / Reading functionality

--- public/app/main.js
+++ public/app/main.js
@@ -20,6 +20,20 @@ p10x.controller('HeaderCtrl', function($scope) {
   $scope.appDetails = {};
   $scope.appDetails.title = "P10X";
   $scope.appDetails.tagline = "Be a 10x programmer";
+  $scope.typingActive = true;
+  $scope.readingActive = false;
+  $scope.toggleTyping = function () {
+    if(!$scope.typingActive) {
+      $scope.typingActive = !$scope.typingActive;
+      $scope.readingActive = !$scope.readingActive;
+    }
+  }
+  $scope.toggleReading = function () {
+    if(!$scope.readingActive) {
+      $scope.typingActive = !$scope.typingActive;
+      $scope.readingActive = !$scope.readingActive;
+    }
+  }
 });
 
 
