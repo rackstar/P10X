e0cfafee950cd40ed1e0a8640339caeb53d91377
Rocky Murdoch <rockysta009@gmail.com>
2016-04-05 22:57:21 +0100

    added animation features
    *keyboard functionality lights up key being pressed
    *compare checks if input is correct or not

--- public/app/typing/typing.js
+++ public/app/typing/typing.js
@@ -1,38 +1,33 @@
 var type = {};
 
 $(function () {
-  var $inputArea = $('#inputArea')
-  type.code = "for (var i = 0; i < array.length; i++)"
-  type.key;
+
+  type.code = "for (var i = 0; i < array.length; i++)";
+
   type.index = 0;
-
-  type.init = function () {
-    //listener for key input
-    $inputArea.on('keydown', function (event) {
-      type.key = String.fromCharCode(event.which);
-      //compares key input to text
-      type.compare(type.key);
-    });
-
-  }
-
-  ;
 
   type.compare = function (key) {
     //compare with current letter
+    $('#' + type.index).removeClass('next');
     if (key === type.code.charAt(type.index)) {
-      //if same light up green
-      $('#' + type.index).addClass('correct');
+      //if same addClass correct
+      $('#' + type.index).addClass('correctType');
     } else {
+      //if different addClass incorrect
       $('#' + type.index).addClass('incorrect');
-      
     }
-
-
-
-    //if different light up red
-
-
+    type.index++;
   }
+
+  type.keyboard = function(key) {
+    $('#' + type.index).addClass('next');
+    var pressedKey = $('#keyboard').find('.' + key);
+    pressedKey.addClass('activeType')
+      .delay(150)
+      .queue(function() {
+        $(this).removeClass("activeType");
+        $(this).dequeue()
+      });
+  };
 
 });
