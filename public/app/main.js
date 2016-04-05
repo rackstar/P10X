d751924ce1cd06fc65e8d379ea73b24faf17e28a
Rocky Murdoch <rockysta009@gmail.com>
2016-04-05 23:29:14 +0100

    added new tab for table with activate functionality
    *migrated schultz table app to table tab

--- public/app/main.js
+++ public/app/main.js
@@ -5,11 +5,15 @@ var p10x = angular.module('p10x', ['ngRoute', 'reading', 'services']);
 p10x.config(function ($routeProvider) {
   $routeProvider
     .when('/reading', {
-      templateUrl: 'app/reading/reading.html',
-      controller: 'readingCtrl'
+      templateUrl: 'app/realReading/read.html',
+      // controller: 'realReadCtrl'
     })
     .when('/typing', {
       templateUrl: 'app/typing/typing.html',
+    })
+    .when('/table', {
+      templateUrl: 'app/reading/reading.html',
+      controller: 'readingCtrl'
     })
     .otherwise({
       redirectTo: '/typing'
@@ -20,20 +24,29 @@ p10x.controller('HeaderCtrl', function($scope) {
   $scope.appDetails = {};
   $scope.appDetails.title = "P10X";
   $scope.appDetails.tagline = "Be a 10x programmer";
+
   $scope.typingActive = true;
   $scope.readingActive = false;
+  $scope.tableActive = false;
+
   $scope.toggleTyping = function () {
-    if(!$scope.typingActive) {
-      $scope.typingActive = !$scope.typingActive;
-      $scope.readingActive = !$scope.readingActive;
-    }
-  }
+    $scope.readingActive = false;
+    $scope.tableActive = false;
+    $scope.typingActive = true;
+  };
+
   $scope.toggleReading = function () {
-    if(!$scope.readingActive) {
-      $scope.typingActive = !$scope.typingActive;
-      $scope.readingActive = !$scope.readingActive;
-    }
-  }
+    $scope.typingActive = false;
+    $scope.tableActive = false;
+    $scope.readingActive = true;
+  };
+
+  $scope.toggleTable = function () {
+    $scope.typingActive = false;
+    $scope.readingActive = false;
+    $scope.tableActive = true;
+  };
+
 });
 
 
