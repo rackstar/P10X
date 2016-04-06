0c9447619aed6bdbad96f37e3577fa912f260e60
Rocky Murdoch <rockysta009@gmail.com>
2016-04-06 04:08:52 +0100

    deleted unneccessary function

--- public/app/read/read.js
+++ public/app/read/read.js
@@ -2,6 +2,5 @@ angular.module('read', [])
 
 .controller('readCtrl', function ($scope, readSrvc) {
   $scope.text = readSrvc.text;
-  $scope.animate = false;
-  // readSrvc.appendData($scope.text);
+
 });
